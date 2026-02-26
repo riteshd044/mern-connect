897ead14ae0e22d2e3dc16393f0ac462e3fdc901
riteshd044 <riteshd044@gmail.com>
2026-02-26 16:24:55 +0530

    1-notification routes and controllers added

--- backend/server.js
+++ backend/server.js
@@ -6,6 +6,7 @@ import { v2 as cloudinary } from "cloudinary";
 import authRoutes from "./routes/auth.route.js"
 import userRoutes from "./routes/user.route.js"
 import postRoutes from "./routes/post.route.js"
+import notificationRoutes from "./routes/notification.route.js"
 import connectDB from "./lib/db.js";
 
 dotenv.config();
@@ -26,6 +27,7 @@ app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/post", postRoutes);
+app.use("/api/notifications", notificationRoutes);
 
 connectDB().then(()=>{
     app.listen(PORT, () =>{
