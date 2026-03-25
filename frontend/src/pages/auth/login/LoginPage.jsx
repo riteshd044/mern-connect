45dc2c053696974160c60b2e74f821cad22061ba
riteshd044 <riteshd044@gmail.com>
2026-03-25 18:40:52 +0530

    3-Auth handled and routes protected in client

--- frontend/src/pages/auth/login/LoginPage.jsx
+++ frontend/src/pages/auth/login/LoginPage.jsx
@@ -5,6 +5,8 @@ import XSvg from "../../../components/svgs/X";
 
 import { MdOutlineMail } from "react-icons/md";
 import { MdPassword } from "react-icons/md";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
 
 const LoginPage = () => {
 	const [formData, setFormData] = useState({
@@ -12,16 +14,39 @@ const LoginPage = () => {
 		password: "",
 	});
 
+	const queryClient = useQueryClient();
+
+	const { mutate:loginMutation, isError, isPending, error } = useMutation({
+		mutationFn: async (formData) => {
+			try{
+				const res = await fetch('/api/auth/login', {
+					method: "POST",
+					headers: { "Content-Type" : "application/json"},
+					body: JSON.stringify(formData)
+				});
+				const data = await res.json();
+				if (!res.ok) throw new Error(data.error || "Something went wrong");
+				return data;
+			}catch(error){
+				console.log(error);
+				throw error;
+			}
+		},
+		onSuccess: (data) => {
+			console.log(data);
+			toast.success("Logged In successfully");
+			queryClient.invalidateQueries({queryKey: ["authUser"]});
+		}
+	});
+
 	const handleSubmit = (e) => {
 		e.preventDefault();
-		console.log(formData);
+		loginMutation(formData);
 	};
 
 	const handleInputChange = (e) => {
 		setFormData({ ...formData, [e.target.name]: e.target.value });
 	};
-
-	const isError = false;
 
 	return (
         <div className='max-w-screen-xl mx-auto flex h-screen px-10'>
@@ -57,8 +82,8 @@ const LoginPage = () => {
 						/>
 					</label>
                     </div>
-					<button className='btn rounded-full btn-primary text-white'>Login</button>
-					{isError && <p className='text-red-500'>Something went wrong</p>}
+					<button className='btn rounded-full btn-primary text-white'>{isPending ? "Loading..." : "Log in"}</button>
+					{isError && <p className='text-red-500'>{error.message}</p>}
 				</form>
 				<div className='flex flex-col lg:w-2/3 gap-2 mt-4'>
 					<p className='text-white text-lg'>{"Don't"} have an account?</p>
